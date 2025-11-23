83d3b190179b35b03d7a059af4029b478e50b469
leo060702 <zzhong@byupathway.edu>
2025-11-22 17:22:33 -0800

    Add files via upload

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,27 +1,70 @@
+// controllers/inventoryController.js
 const invModel = require("../models/inventory-model");
 const utilities = require("../utilities");
 
-/* Build the vehicle detail view */
-async function buildDetailView(req, res, next) {
+/**
+ * 分类视图 /inv/type/:classificationId
+ */
+async function buildByClassificationId(req, res, next) {
+  const classificationId = Number(req.params.classificationId);
+  if (Number.isNaN(classificationId)) {
+    const err = new Error("Invalid classification id");
+    err.status = 400;
+    return next(err);
+  }
+
   try {
-    const invId = req.params.invId;
-    const data = await invModel.getVehicleById(invId);
+    const data = await invModel.getInventoryByClassificationId(classificationId);
+    const nav = await utilities.getNav();
+    const grid = await utilities.buildClassificationGrid(data);
+    const className = data[0]?.classification_name || "Vehicles";
 
-    if (!data) {
-      // Throw 404 if no data found
-      const err = new Error("Vehicle not found");
-      err.status = 404;
-      throw err;
-    }
-
-    const content = utilities.buildVehicleHTML(data);
-    res.render("inventory/detail", {
-      title: `${data.inv_make} ${data.inv_model}`,
-      content,
+    res.render("inventory/classification", {
+      title: className,
+      nav,
+      grid,
+      errors: null,
     });
   } catch (error) {
     next(error);
   }
 }
 
-module.exports = { buildDetailView };
+/**
+ * 车辆详情视图 /inv/detail/:invId
+ */
+async function buildDetailView(req, res, next) {
+  const invId = Number(req.params.invId);
+  if (Number.isNaN(invId)) {
+    const err = new Error("Invalid vehicle id");
+    err.status = 400;
+    return next(err);
+  }
+
+  try {
+    const vehicle = await invModel.getVehicleById(invId);
+    if (!vehicle) {
+      const err = new Error("Vehicle not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    const nav = await utilities.getNav();
+    const content = utilities.buildVehicleHTML(vehicle);
+    const title = `${vehicle.inv_make} ${vehicle.inv_model}`;
+
+    res.render("inventory/detail", {
+      title,
+      nav,
+      content,
+      errors: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  buildByClassificationId,
+  buildDetailView,
+};
